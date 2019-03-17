128b52e30193450e85ce6974cf667befd48b2a81
NatanaelA <natanaela@xrvision.com>
2019-03-17 11:03:16 +0800

    store and read value

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("192.168.99.100");
+            Console.WriteLine("Connected!");
+
+            IDatabase db = redis.GetDatabase();
+
+            string value = "abcdefg";
+            db.StringSetAsync("mykey", value, flags: CommandFlags.FireAndForget);
+            Console.WriteLine("mykey is stored!");
+
+            string valueStored = db.StringGet("mykey");
+            Console.WriteLine("Read mykey from redis: " + valueStored);
+
+            Console.ReadLine();
+        }
+    }
+}
